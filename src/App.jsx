24b27022e2bd876a6b537407da4b03894f81b304
Allan Chementer <allanchementer@gmail.com>
2025-03-05 10:27:23 +0300

    fixing 404 error

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Works from "./Pages/Works";
 import Blogs from "./Pages/Blogs";
 import Contact from "./Pages/Contact";
 import WorkDetail from "./Pages/WorkDetail";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./index.css";
 
 function App() {
   return (
-    <HashRouter>
+    <>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/works" element={<Works />} />
@@ -16,7 +16,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/works/:id" element={<WorkDetail />} />
         </Routes>
-    </HashRouter>
+    </>
   );
 }
 

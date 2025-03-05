526bf67c38cc6e578b734eacc81e63bd73db8a09
Allan Chementer <allanchementer@gmail.com>
2025-03-05 10:20:05 +0300

    fixing 404 error

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Portfolio/",
+  server: {
+    historyApiFallback: true,
+  },
   plugins: [react(), tailwindcss()],
 });
